48c2627fa0d04183c1d4f733d785b79d12c57f2b
bryantsands <bryantsands@bryantsands.local>
2017-03-02 13:08:15 -0800

    functionality

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Router = require("react-router").Router;
+
+var routes = require("./components/config/routes");
+
+// Render the Main component
+ReactDOM.render(
+	routes, document.getElementById("app")
+	);
